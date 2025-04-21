b45270a5b2c2071c09411e535dfdb72be665db45
unknown <mdameeruddinahmed@gmail.com>
2025-04-21 14:21:09 +0530

    Links app update

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/tech" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about-us" element={<AboutUs />} />
           <Route path="all-product" element={< AllProduct />} />
@@ -40,27 +40,27 @@ function App() {
           <Route path="career/1" element={<CareerDescription />} />
           <Route path="career/2" element={<CareerDescrip />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="/software-services" element={<SoftwareServices />} />
-          <Route path="/hardware-services" element={<HardwareServices />} />
-          <Route path="/media-services" element={<MediaServices />} />
-          <Route path="/software-services/custom-desktop-app" element={<CustomApp />} />
-          <Route path="/software-services/web-applications" element={<WebApp />} />
-          <Route path="/software-services/mobile-applications" element={<MobileApp />} />
-          <Route path="/property-management" element={<PropertyManagementSoftware />} />
-          <Route path="/property-crm" element={<PropertyCRMTool/>} />
-          <Route path="/property-broker" element={ <PropertyBrokerManagement/>} />
-          <Route path="/work-order" element={<WorkOrderManagement/>} />
-          <Route path="/inventory-management" element={<InventoryManagement/>} />
-          <Route path="/maid-service" element={<MaidServiceManagement/>} />
-          <Route path="/electronic-service" element={<ElectronicServiceCenterManagement/>} />
-          <Route path="/centralised-document" element={<CentralisedDocumentManagement/>} />
-          <Route path="/auto-garage" element={<AutogarageServiceCenterManagement/>} />
-          <Route path="/hardware-services/server" element={<Server/>} />
-          <Route path="/hardware-services/biometric" element={<Biometric/>} />
-          <Route path="/hardware-services/cctv" element={<CCTV/>} />
-          <Route path="/media-services/seo" element={<SEO/>} />
-          <Route path="/media-services/brochure" element={<Brochure/>} />
-          <Route path="/media-services/webServices" element={<WebServices/>} />
+          <Route path="software-services" element={<SoftwareServices />} />
+          <Route path="hardware-services" element={<HardwareServices />} />
+          <Route path="media-services" element={<MediaServices />} />
+          <Route path="software-services/custom-desktop-app" element={<CustomApp />} />
+          <Route path="software-services/web-applications" element={<WebApp />} />
+          <Route path="software-services/mobile-applications" element={<MobileApp />} />
+          <Route path="property-management" element={<PropertyManagementSoftware />} />
+          <Route path="property-crm" element={<PropertyCRMTool/>} />
+          <Route path="property-broker" element={ <PropertyBrokerManagement/>} />
+          <Route path="work-order" element={<WorkOrderManagement/>} />
+          <Route path="inventory-management" element={<InventoryManagement/>} />
+          <Route path="maid-service" element={<MaidServiceManagement/>} />
+          <Route path="electronic-service" element={<ElectronicServiceCenterManagement/>} />
+          <Route path="centralised-document" element={<CentralisedDocumentManagement/>} />
+          <Route path="auto-garage" element={<AutogarageServiceCenterManagement/>} />
+          <Route path="hardware-services/server" element={<Server/>} />
+          <Route path="hardware-services/biometric" element={<Biometric/>} />
+          <Route path="hardware-services/cctv" element={<CCTV/>} />
+          <Route path="media-services/seo" element={<SEO/>} />
+          <Route path="media-services/brochure" element={<Brochure/>} />
+          <Route path="media-services/webServices" element={<WebServices/>} />
           <Route path="*" element={<h1>404</h1>} />
         </Route>
       </Routes>
